e7bddb90fc04a0d415358f306b113652d22ee632
ITJANG <whalswkd1213@gmail.com>
2025-05-23 10:24:04 +0900

    0523

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Exam1 from './components/Exam1';
 import Exam2 from './components/Exam2';
+import Exam3 from './components/Exam3';
+import Exam4 from './components/Exam4';
+import Exam5 from './components/Exam5';
+import Exam6 from './components/Exam6';
+import Exam7 from './components/Exam7';
+import TodoList from './components/TodoList';
 
 function App() {
   // 상태(state)
@@ -19,14 +25,20 @@ function App() {
     //   {/* jsx 주석 */}
     // </>
 
-    <>
-      <button onClick={() => setShoExam(!showExam)}>클릭</button>
+    // <>
+    //   <button onClick={() => setShoExam(!showExam)}>클릭</button>
 
-      {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
-      {showExam && <Exam2 minjang="hello" test="world"/>}
-    </>
+    //   {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
+    //   {showExam && <Exam2 minjang="hello" test="world"/>}
+    // </>
 
-  )
+    // <>
+    //   <Exam7></Exam7>
+    // </>
+
+    <TodoList></TodoList>
+
+  );
 }
 
 export default App
